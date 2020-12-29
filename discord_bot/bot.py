ad1ab5fe3a9680e3ddb421996a6ec0a7c092a51f
Charlie Friend <charles.d.friend@gmail.com>
2020-12-28 20:57:15 -0800

    Basic discord bot config

---
+++ discord_bot/bot.py
@@ -0,0 +1,47 @@
+import sys
+import discord
+import logging
+from discord_bot.config import DISCORD_BOT_TOKEN
+
+client = discord.Client()
+
+# Configure logging
+root = logging.getLogger()
+root.setLevel(logging.INFO)
+
+handler = logging.StreamHandler(sys.stdout)
+root.addHandler(handler)
+
+
+def is_mentioned(message) -> bool:
+    """Returns true if our user is mentioned in a message. Excludes the @everyone role."""
+    if client.user.id in message.raw_mentions:
+        return True
+
+    # Are any of my roles mentioned?
+    if message.guild:
+        roles = message.guild.me.roles
+        for role in roles:
+            # Ignore default @everyone role
+            if role == message.guild.default_role:
+                continue
+
+            if role.id in message.raw_role_mentions:
+                return True
+
+    return False
+
+
+@client.event
+async def on_ready():
+    logging.info(f"Logged in as {client.user}")
+
+
+@client.event
+async def on_message(message):
+    if is_mentioned(message):
+        await message.channel.send("sup")
+
+
+if __name__ == "__main__":
+    client.run(DISCORD_BOT_TOKEN)
--- discord_bot/config.py
+++ discord_bot/config.py
@@ -3,3 +3,6 @@ import os
 # IsThereAnyDeal params
 ITAD_API_KEY = os.environ.get("ITAD_API_KEY")
 ITAD_REGION = os.environ.get("ITAD_REGION")
+
+# Discord Params
+DISCORD_BOT_TOKEN = os.environ.get("DISCORD_BOT_TOKEN")
